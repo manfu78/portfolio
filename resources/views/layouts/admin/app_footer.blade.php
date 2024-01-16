1ad8c964c8eb9b957ba260e351414a8a5cd6770d
Germán Raúl <germansoy@gmail.com>
2024-01-16 22:32:48 +0100

    Create Menu Tables Module

--- resources/views/layouts/admin/app_footer.blade.php
+++ resources/views/layouts/admin/app_footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row align-items-center flex-row-reverse">
             <div class="col-md-12 col-sm-12 text-center">
-                Copyright © <span id="year"></span> <a href="javascript:void(0)">AppSPMAgency</a>.&nbsp;{{ trans('messages.DesignedWith') }}&nbsp;<span class="fa fa-heart text-danger"></span>&nbsp;&&nbsp;{{ trans('messages.PublicBy') }}&nbsp;<a href="https://agenciaspm.es/"> Agencia SPM - Marketing Hibrido </a> All rights reserved.
+                Copyright © <span id="year"></span> <a href="javascript:void(0)">Windvalley</a>.&nbsp;{{ trans('messages.DesignedWith') }}&nbsp;<span class="fa fa-heart text-danger"></span>&nbsp;&&nbsp;{{ trans('messages.PublicBy') }}&nbsp;<a href="https://germanraulgarcia.es/"> Germán Raúl García </a> All rights reserved.
             </div>
         </div>
     </div>
